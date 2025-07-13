acaf1d90af46fcc875b3e04b0fccbe7140aad2f7
vkcku <git@mail.vkcku.com>
2025-07-13 09:09:36 +0530

    home(shell): enable fzf integration with zsh

--- home/shell/shell.nix
+++ home/shell/shell.nix
@@ -14,6 +14,11 @@
     enableZshIntegration = true;
   };
 
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
